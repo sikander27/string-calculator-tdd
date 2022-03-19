a3e8f1f2b84e948a0afdcd4b5af121a679604d4b
sikander27 <imsikandarkhan@outlook.com>
2022-03-19 22:15:17 +0530

    code refractoring done

--- string_calculator.py
+++ string_calculator.py
@@ -4,11 +4,6 @@ import re
 def add(s):
     if s == "": return 0
     numbers = list(map(int, re.findall(r"-?\d+", s)))
-    # import pdb; pdb.set_trace()
-    negative_numbers = []
-    for i in numbers:
-        if i < 0:
-            negative_numbers.append(i)
-    if negative_numbers:
-        raise Exception('negatives not allowed {}'.format(negative_numbers))
+    negative_numbers = list(filter(lambda x: x < 0, numbers))
+    if negative_numbers: raise Exception('negatives not allowed {}'.format(negative_numbers))
     return sum(numbers)
