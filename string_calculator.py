10eb54f5bb661b02dcb23dfdb49658219b13edb2
sikander27 <imsikandarkhan@outlook.com>
2022-03-19 20:17:26 +0530

    1. Added basic test cases

--- string_calculator.py
+++ string_calculator.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
-def main():
+def string_calculator(s):
     print("String Calculator")
+    if s == "":
+        return 0
+    numbers = list(map(int, s.split(",")))
+    return sum(numbers)
--- test_calcluator.py
+++ test_calcluator.py
@@ -1,8 +1,11 @@
 """
 Test suit for string_calculator.py
 """
+from string_calculator import string_calculator
 
-def test_nothing():
-    pass
+def test_string_calculator():
+    assert string_calculator("") == 0
+    assert string_calculator("2") == 2
+    assert string_calculator("1,2") == 3
 
 
